0749158e99c2fbd702e039c009f76537c0cf7581
Gabriel <gabrielosantosb@gmail.com>
2024-11-28 22:07:55 -0300

    pegando leadsData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import {NZ_DATE_LOCALE, NZ_I18N, pt_BR} from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ManageLeadComponent } from './pages/manage-lead/manage-lead.component';
+import {NzPageHeaderModule} from "ng-zorro-antd/page-header";
 
 registerLocaleData(en);
 
@@ -24,10 +25,13 @@ registerLocaleData(en);
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzPageHeaderModule
   ],
   providers: [
-    { provide: NZ_I18N, useValue: en_US }
+    { provide: NZ_I18N, useValue: pt_BR },
+    { provide: NZ_DATE_LOCALE, useValue: pt_BR },
+    { provide: LOCALE_ID, useValue: "pt-BR" },
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/manage-lead/manage-lead.component.ts
+++ src/app/pages/manage-lead/manage-lead.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {catchError, finalize, Subject, takeUntil, throwError} from "rxjs";
+import {LeadService} from "../../shared/service/lead.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {PaginationModel} from "../../shared/model/Pagination/pagination.model";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MessageService} from "../../shared/utils/messageService";
+import {LeadModel} from "../../shared/interface/lead.model";
 
 @Component({
   selector: 'app-manage-lead',
   templateUrl: './manage-lead.component.html',
   styleUrls: ['./manage-lead.component.scss']
 })
-export class ManageLeadComponent {
+export class ManageLeadComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  filterLeadForm!: FormGroup;
+  createLeadForm!: FormGroup
+  leadsData !: LeadModel[]
+  pagination = { page: 1, pageSize: 10 };
+  isLoading: boolean = false;
 
+
+  constructor(private leadService: LeadService, private formBuilder: FormBuilder, private messageService: MessageService) {
+  }
+
+  ngOnInit(): void {
+   this.loadInstances()
+   this.loadLeads()
+  }
+
+  private loadLeads(){
+    const filterFormValue = this.filterLeadForm.value;
+    const cleanedFilters = this.cleanObject({ ...filterFormValue, ...this.pagination });
+
+
+    this.leadService.getLeads(cleanedFilters, this.pagination).pipe(takeUntil(this.destroy$),
+      finalize(() => this.isLoading = false),
+      catchError((err: HttpErrorResponse) => {
+        const errorMessage = err.error?.message || 'Erro ao carregar leads!';
+        console.error(err);
+        this.messageService.errorMessage(errorMessage);
+        return throwError(() => err);
+      }),
+      ).subscribe((res) =>{
+        this.leadsData = res
+        console.log('AQUI LEADSDATA', this.leadsData)
+    })
+
+  }
+  private loadInstances(){
+    this.createLeadForm = this.formBuilder.group({
+      contactFirstName: ["", Validators.required],
+      contactLastName: ["", Validators.required],
+      contactEmail: ["", Validators.required],
+      contactPhone: ["", Validators.required],
+      suburb: ["", Validators.required],
+      category: ["", Validators.required],
+      description: ["", Validators.required],
+      price: [0, Validators.required],
+
+
+    })
+
+
+    this.filterLeadForm = this.formBuilder.group({
+      contactFirstName: [""],
+      contactLastName: [""],
+      contactEmail: [""],
+      suburb: [""],
+      category: [""],
+      dateCreatedStart: [""],
+      dateCreatedEnd: [""],
+      minPrice: [0],
+      maxPrice: [0],
+    })
+  }
+
+  // função para limpar objetos dos filtros, removendo valores nulos, vazios ou padrão
+  private cleanObject(obj: any): any {
+    return Object.fromEntries(
+      Object.entries(obj).filter(([key, value]) => {
+        // Filtrar valores nulos, strings vazias e 0, exceto para paginação
+        if (['page', 'pageSize'].includes(key)) {
+          return true; // sempre manter `page` e `pageSize`
+        }
+        return value !== null && value !== undefined && value !== '' && value !== 0;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }

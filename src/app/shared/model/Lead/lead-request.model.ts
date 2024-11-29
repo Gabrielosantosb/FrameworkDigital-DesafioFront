82d329600208ec48852800d9ce05d1be610d73de
Gabriel <gabrielosantosb@gmail.com>
2024-11-28 21:09:47 -0300

    Definindo models

---
+++ src/app/shared/model/Lead/lead-request.model.ts
@@ -0,0 +1,12 @@
+export interface LeadRequest {
+  contactFirstName : string;
+  contactLastName : string;
+  contactEmail : string;
+  contactPhoneNumber : string;
+  suburb: string;
+  category: string;
+  description: string;
+  price: string;
+  status: string;
+
+}
